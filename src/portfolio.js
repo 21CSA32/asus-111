0e0355ed3ab917b049f3c7467cca82171b859e9e
Litheeshkumar G <21csa32@karpagamtech.ac.in>
2024-05-01 09:57:21 +0530

    Barath updated

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,8 +6,8 @@ const header = {
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Litheeshkumar G',
+  role: 'DEVOPS Engineer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
@@ -54,18 +54,12 @@ const skills = [
   'JavaScript',
   'TypeScript',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'litheeshkumar93@mail.com',
 }
 
 export { header, about, projects, skills, contact }
